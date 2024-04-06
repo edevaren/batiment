9c3245b5992a37c0bdb85acbd3df68ea529be0d6
edevaren <enguerrand.devarenne@ecl20.ec-lyon.fr>
2024-04-06 11:29:31 +0200

    suite du commentaire

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import RoomList from "./components/rooms/RoomList";
 import classes from "./App.module.css";
 import axios from "axios";
 
-//fonction pour récupérer les données de l'API
+//fonction pour récupérer les données de l'API et c'est déjà bien
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedRooms1, setLoadedRooms1] = useState([]);
